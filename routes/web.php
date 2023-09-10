410aea214aa51fa865a3821b345c7cd3e2b6dfe7
Mahdi <rahmanimahdi16@gmail.com>
2023-09-10 11:36:26 +0330

    before breeze

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,7 +19,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::prefix('auth')->controller(\App\Http\Controllers\RegisterController::class)->group(function () {
-   Route::get('/register','showRegistrationForm')->name('auth.register.form');
-   Route::get('/register','register')->name('auth.register');
+Route::prefix('auth')->group(function () {
+  Route::controller(RegisterController::class)->group(function () {
+      Route::get('/register','showRegistrationForm')->name('auth.register.form');
+      Route::get('/register','register')->name('auth.register');
+  });
+
+  Route::controller(LoginController::class)->group(function (){
+     Route::get('/login','showLoginForm')->name('auth.login.form');
+     Route::post('/login','login')->name('auth.login');
+  });
 });
